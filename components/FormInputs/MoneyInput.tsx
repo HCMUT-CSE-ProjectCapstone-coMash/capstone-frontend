27b0dc660b466d1ff22fc697e8f88602e30ccc46
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-03-18 22:04:33 +0700

    fix: style MoneyInput

--- components/FormInputs/MoneyInput.tsx
+++ components/FormInputs/MoneyInput.tsx
@@ -19,7 +19,7 @@ export function MoneyInput({ label, placeHolder, value, onChange, inputType="tex
                     {label}
                 </label>
             )}
-            <div className={`flex items-center bg-white
+            <div className={`flex items-center bg-white text-sm
                 ${isRight ? 'w-30' : 'w-full'} 
                 px-3 py-2 rounded-lg border-[0.5px] border-solid border-tgray5 
                 focus-within:border-purple focus-within:ring-1 focus-within:ring-purple transition-all
@@ -31,7 +31,7 @@ export function MoneyInput({ label, placeHolder, value, onChange, inputType="tex
                     onChange={onChange} 
                     className="w-full bg-transparent focus:outline-none"
                 />
-                <span className="ml-1 text-black font-normal">
+                <span className="ml-1 text-black text-sm font-normal">
                     VND
                 </span>
             </div>
