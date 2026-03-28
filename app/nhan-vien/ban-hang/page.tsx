f0ef99d25859f51dadc961c40761a8724b2c7b3b
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-03-29 01:26:52 +0700

    feature: Bán hàng thanh search

--- app/nhan-vien/ban-hang/page.tsx
+++ app/nhan-vien/ban-hang/page.tsx
@@ -1,6 +1,6 @@
 import { InvoiceForm } from "@/components/Forms/InvoiceForm";
 import { SellProductsTable } from "@/components/Tables/SellProductsTable";
-import { SearchIcon } from "@/public/assets/Icons";
+import { InvoiceSearch } from "@/components/InvoiceSearch";
 
 
 export default function SellPage() {
@@ -11,16 +11,7 @@ export default function SellPage() {
             {/* Header: Tiêu đề và Thanh tìm kiếm */}
             <div className="flex justify-between items-center mb-6">
                 <p className="text-purple text-2xl font-medium">Bán hàng</p>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <SearchIcon width={24} height={24} className="text-tgray9" />
-                    </div>
-                    <input
-                        type="text"
-                        className="block w-80 py-2.5 pl-12 pr-4 text-sm border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-purple-500"
-                        placeholder="Nhập số điện thoại khách hàng"
-                    />
-                </div>
+                <InvoiceSearch/>
             </div>
             <div className="flex flex-row gap-16">
             
