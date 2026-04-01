7e47393852a998146104e7e26059d550492d0cc4
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-04-01 14:55:44 +0700

    feature: Chủ cửa hàng/ Quản lý nhân viên - tạo trang thêm nhân viên

--- app/chu-cua-hang/quan-ly-nhan-vien/page.tsx
+++ app/chu-cua-hang/quan-ly-nhan-vien/page.tsx
@@ -1,17 +1,20 @@
+import Link from "next/link";
 import { EmployeeSearch } from "@/components/SearchBar/EmployeeSearch";
 import { EmployeeTable } from "@/components/Tables/EmployeeTable";
+import { OwnerEmployeeManagementPageRoute } from "@/const/routes";
 
-export default function Employee() {
+export default function EmployeeManagement() {
     return (
         <main className="px-20 py-5">
             <div className="text-purple text-3xl font-medium">Danh sách nhân viên</div>
             <div className="my-10.25 flex flex-column gap-10 justify-end">
                 <EmployeeSearch/>
-                <button
-                            className={`py-2 px-3 rounded-lg text-white font-semibold bg-purple text-sm cursor-pointer`}
-                        >
-                            Thêm nhân viên mới
-                </button>
+                <Link
+                    href={`${OwnerEmployeeManagementPageRoute}/them-nhan-vien`}
+                    className="py-2 px-3 rounded-lg text-white font-semibold bg-purple text-sm cursor-pointer inline-block text-center"
+                >
+                    Thêm nhân viên mới
+                </Link>
             </div>
             <EmployeeTable/>
         </main>
---
+++ app/chu-cua-hang/quan-ly-nhan-vien/them-nhan-vien/page.tsx
@@ -0,0 +1,8 @@
+
+export default function EmployeeAdd() {
+    return (
+        <main className="px-20 py-5">
+            <p>Hi</p>
+        </main>
+    );
+}
