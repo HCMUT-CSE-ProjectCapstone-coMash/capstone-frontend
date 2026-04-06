d02bc9ebf2897440ad02e00f66bbfd6566db0671
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-04-06 13:15:53 +0700

    feature: Quản lý nhân viên

--- app/chu-cua-hang/quan-ly-nhan-vien/page.tsx
+++ app/chu-cua-hang/quan-ly-nhan-vien/page.tsx
@@ -1,22 +1,17 @@
-import Link from "next/link";
-import { EmployeeSearch } from "@/components/SearchBar/EmployeeSearch";
+
 import { EmployeeTable } from "@/components/Tables/EmployeeTable";
-import { OwnerEmployeeManagementPageRoute } from "@/const/routes";
 
 export default function EmployeeManagement() {
     return (
         <main className="px-20 py-5">
             <div className="text-purple text-3xl font-medium">Danh sách nhân viên</div>
-            <div className="my-10.25 flex flex-column gap-10 justify-end">
-                <EmployeeSearch/>
-                <Link
-                    href={`${OwnerEmployeeManagementPageRoute}/them-nhan-vien`}
-                    className="py-2 px-3 rounded-lg text-white font-semibold bg-purple text-sm cursor-pointer inline-block text-center"
-                >
-                    Thêm nhân viên mới
-                </Link>
-            </div>
-            <EmployeeTable/>
+            
+            {/* Chỉ còn lại thanh tìm kiếm ở phía trên
+            <div className="my-10 flex justify-end">
+                <EmployeeSearch />
+            </div> */}
+            
+            <EmployeeTable />
         </main>
     );
 }
