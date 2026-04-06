5f97b1ce9852f24e61bcb3f6e7880f807047b949
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-04-06 15:13:06 +0700

    Feature: Nhân viên - bán hàng - cập nhật bảng

--- app/nhan-vien/ban-hang/page.tsx
+++ app/nhan-vien/ban-hang/page.tsx
@@ -1,15 +1,19 @@
 import { InvoiceForm } from "@/components/Forms/InvoiceForm";
 import { SellProductsTable } from "@/components/Tables/SellProductsTable";
-import { InvoiceSearch } from "@/components/SearchBar/InvoiceSearch";
+// import { InvoiceSearch } from "@/components/SearchBar/InvoiceSearch";
 
 
 export default function SellPage() {
     return (
         <main className="px-20 pt-10 pb-25">
             <p className="text-purple text-3xl font-medium">Bán hàng</p>
-            <div className="flex gap-[10vw]">
-                <p className="text-2xl font-medium">Bảng</p>
-                <InvoiceForm/>
+            <div className="flex justify-between gap-[10vw]">
+                <div className="w-1/3">
+                    <SellProductsTable/>
+                </div>
+                <div className="w-2/3">
+                    <InvoiceForm/>
+                </div>
             </div>
         </main>
     );
