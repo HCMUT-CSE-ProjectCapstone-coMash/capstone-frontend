514b8917fd8636188c09ef692d471e472dc90a8e
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-04-17 16:38:19 +0700

    feature: Qly nhân viên/ update EmployeeDetailPage

--- app/chu-cua-hang/quan-ly-nhan-vien/chi-tiet/[employeeId]/page.tsx
+++ app/chu-cua-hang/quan-ly-nhan-vien/chi-tiet/[employeeId]/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import Link from "next/link";
+import { OwnerEmployeeManagementPageRoute } from "@/const/routes";
 
 export default function EmployeeDetailPage() {
 
     return (
-        <main className="px-20 pt-10 pb-25">
-            Hi
+        <main className="px-20 py-10">
+            <div className="flex justify-between items-center mb-12.5">
+                <div className="text-purple text-3xl font-medium">Nhân viên</div>
+                <Link
+                    href={`${OwnerEmployeeManagementPageRoute}`}
+                    className="border border-purple text-purple font-medium px-3 py-2 rounded-lg text-sm cursor-pointer inline-block text-center hover:bg-purple-50"
+                >
+                    Danh sách nhân viên
+                </Link>
+            </div>
         </main>
     )
 }
