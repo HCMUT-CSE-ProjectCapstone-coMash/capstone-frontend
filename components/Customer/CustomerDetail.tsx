6b6f4bafe4609bcb215266eb18976c6a4c775e4e
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-04-28 19:53:39 +0700

    Update CustomerDetail

--- components/Customer/CustomerDetail.tsx
+++ components/Customer/CustomerDetail.tsx
@@ -14,46 +14,58 @@ interface CustomerDetailProps {
 export function CustomerDetail({ customer }: CustomerDetailProps) {
 
     return (
-        <div className="flex flex-col gap-8">
+        <div>
+            <div className="flex justify-between items-center mb-2">
+                <p className="text-lg" >Thông tin khách hàng</p>
+                <button
+                    type="button"
+                    // onClick={}
+                    className="border border-pink text-pink font-medium px-3 py-2 rounded-lg text-sm cursor-pointer hover:bg-pink-50 transition-all"
+                >
+                    Cập nhật nợ
+                </button>
+            </div>
+            <div className="flex flex-col gap-8">
             {/* ── THÔNG TIN KHÁCH HÀNG ── */}
-            <div className="flex flex-col gap-5">
-                <TextInput
-                    disabled
-                    label="Tên khách hàng"
-                    placeHolder=""
-                    value={customer.customerName}
-                    onChange={() => {}}
-                />
-                <TextInput
-                    disabled
-                    label="Số điện thoại"
-                    placeHolder=""
-                    value={customer.customerPhone}
-                    onChange={() => {}}
-                />
+                <div className="flex flex-col gap-5">
+                    <TextInput
+                        disabled
+                        label="Tên khách hàng"
+                        placeHolder=""
+                        value={customer?.customerName || "Khách vãng lai"}
+                        onChange={() => {}}
+                    />
+                    <TextInput
+                        disabled
+                        label="Số điện thoại"
+                        placeHolder=""
+                        value={customer?.customerPhone || ""}
+                        onChange={() => {}}
+                    />
 
-                <div className="flex items-center gap-5">
-                    <div className="w-1/2">
+                    <div className="flex items-center gap-5">
+                       
                         <TextInput
                             disabled
                             label="Số tiền nợ"
                             placeHolder=""
-                            value={formatThousands(customer.debitMoney)}
+                            value={`${formatThousands(customer?.debitMoney || 0)} VNĐ`}
                             onChange={() => {}}
                         />
-                    </div>
-                    <div className="w-1/2">
-                        <TextInput
-                            disabled
-                            label="Số ngày nợ"
-                            placeHolder=""
-                            value={customer.debitDays ? `${customer.debitDays} ngày` : "-"}
-                            onChange={() => {}}
-                        />
+                        
+                        <div className={`w-full ${(customer?.debitDays ?? 0) >= 7 ? "[&_input]:text-red" : ""}`}>
+                            <TextInput
+                                disabled
+                                label="Số ngày nợ"
+                                placeHolder=""
+                                value={`${customer?.debitDays || 0} ngày`}
+                                onChange={() => {}}
+                            />
+                        </div>
+
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
