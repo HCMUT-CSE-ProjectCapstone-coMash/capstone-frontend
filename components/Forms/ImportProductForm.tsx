b68fe5aff4485cfe779e94b152b162b9a7b4a00f
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2026-05-06 13:23:34 +0700

    Update ImportForm của nhân viên

--- components/Forms/ImportProductForm.tsx
+++ components/Forms/ImportProductForm.tsx
@@ -337,7 +337,15 @@ export function ImportProductForm() {
                         ))}
                     </div>
 
-                    <div className="flex justify-end mt-5">
+                    <div className="flex justify-end mt-5 gap-x-6">
+                        <button
+                            type="button"
+                            className="py-2 px-3 rounded-lg text-white bg-purple text-sm cursor-pointer"
+                            onClick={() => setForm(initialFormState)}
+                        >
+                            Huỷ bỏ
+                        </button>
+
                         <button className={`
                             py-2 px-3 rounded-lg text-white bg-pink text-sm
                             ${createMutation.isPending ? "cursor-not-allowed opacity-60" : "cursor-pointer"}`} 
